3437b1c7c5b4cff7d0f8188a17c0d442838c3649
Aydan <aydan2903@gmail.com>
2022-10-12 18:40:48 +0400

    changed dropdown

--- src/companents/ProductsFilterInfo.jsx
+++ src/companents/ProductsFilterInfo.jsx
@@ -1,10 +1,24 @@
-import Dropdown from "../service/Dropdown";
+
 import { useState } from "react";
 import uniqid from "uniqid";
 
 const keywords = ["Tümü", " Yeni gelenler", "İndirimler", "Çok satanlar"];
+const options = [
+  { label: " Sıralama secin", value: " sıralama secin" },
+  { label: "En yüksek fiyat", value: "En yüksek fiyat" },
+  { label: "En yeniler", value: "En yeniler" },
+  { label: "En çok satanlar", value: "En çok satanlar" },
+  { label: "En çok beğenilenler", value: "En çok beğenilenler" },
+  { label: "En çok değerlendirilenler", value: "En çok değerlendirilenler" },
+];
 export default function ProductsFilterInfo() {
   const [active, setActive] = useState("Tümü");
+  const [value, setValue] = useState();
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <div className="bg-white rounded-2xl flex px-4 py-3 justify-between items-center">
       <div className="flex items-center ">
@@ -37,7 +51,24 @@ export default function ProductsFilterInfo() {
           );
         })}
       </div>
-      <Dropdown />
+      <Dropdownn
+        
+        options={options}
+        value={value}
+        onChange={handleChange}
+      />
     </div>
   );
 }
+const Dropdownn = ({ label, value, options, onChange }) => {
+  return (
+    <label>
+      {label}
+      <select value={value} onChange={onChange} className="border-[1px] rounded-lg mx-1 border-[#C4C4C4B2] outline-0 p-2">
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select>
+    </label>
+  );
+};
